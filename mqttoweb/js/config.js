36835f144b2233ae27a081ba2fe669eb7c8dece2
murzhugit <murphy.x.zhu@ericsson.com>
2017-03-10 18:51:09 +0800

    add some samples

---
+++ mqttoweb/js/config.js
@@ -0,0 +1,7 @@
+host = '127.0.0.1';	// hostname or IP address
+port = 21884;
+topic = 'mqtt/mphtopic';       // topic to subscribe to
+useTLS = false;
+username = null;
+password = null;
+cleansession = true;
